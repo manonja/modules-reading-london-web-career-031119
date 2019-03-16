67df2b08163fa343d7fb760ddc79a059b2bb1f22
Manon Jacquin <manon.jacquin@gmail.com>
2019-03-16 16:59:04 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,20 @@
+# we create a module Dance so it's accessible to other classes but other classes
+# don't ihneritate directly from it.
+
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+# we define the kid class and tell it to include the capabilities of the dance module
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
